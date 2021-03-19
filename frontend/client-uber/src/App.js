9ca90f37b469e7ae53ff82ff2f85dfea93ae8a27
SajalSood <sajal.sood@gmail.com>
2021-03-19 11:37:43 -0400

    navbar

--- frontend/client-uber/src/App.js
+++ frontend/client-uber/src/App.js
@@ -1,38 +1,54 @@
 import React, { useState } from 'react';
-import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Dashboard from './components/dashboard';
 import Login from './components/login';
 import Session from './services/session';
+import {
+  Collapse,
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarText,
+  Button
+} from 'reactstrap';
 
 function App() {
   const { user, setUser, clearSession } = Session();
 
   if(!user) {
     return (
-      <div className="App">
-        <header className="App-header">
-          <div className="App-heading">Devops UBER</div>
-        </header>
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">DevopsUBER</NavbarBrand>
+        </Navbar>
         <Login setUser={setUser} />
-      </div>
+    </div>
     )
   }
   
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="App-user">Welcome, {user.username}</div>
-        <div className="App-heading">Devops UBER</div>
-        <button className="App-logout" onClick={e => clearSession()}>Logout</button>
-      </header>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/">
-            <Dashboard user={user}/>
-          </Route>
-        </Switch>
+    <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">DevopsUBER</NavbarBrand>
+          <Collapse navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink href="/">Dashboard</NavLink>
+              </NavItem>
+            </Nav>
+            <NavbarText>Hello, {user.username} &nbsp;&nbsp;&nbsp;</NavbarText>
+            <Button color="danger" onClick={e => clearSession()}>Logout</Button>
+          </Collapse>
+        </Navbar>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/">
+              <Dashboard user={user}/>
+            </Route>
+          </Switch>
       </BrowserRouter>
     </div>
   );
