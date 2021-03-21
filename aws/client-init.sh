733932fa21e982e908f54766b3b84dea8c0e22d7
SajalSood <sajal.sood@gmail.com>
2021-03-20 20:10:31 -0400

    setup apache config

--- aws/client-init.sh
+++ aws/client-init.sh
@@ -2,11 +2,28 @@
 sudo apt-get update
 echo "REACT_APP_SERVER_API_BASE_URL=http://${REACT_APP_SERVER_API_BASE_URL}" >> /etc/environment
 echo "REACT_APP_SERVER_API_PORT=${REACT_APP_SERVER_API_PORT}" >> /etc/environment
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
+sudo apt-get install -y npm
+sudo apt-get install -y apache2
 sudo systemctl start apache2
 sudo systemctl enable apache2
+sudo a2enmod rewrite
+sudo chmod 777 /etc/apache2/sites-available/000-default.conf
+sudo truncate -s0 /etc/apache2/sites-available/000-default.conf
+sudo echo "<VirtualHost *:80>" > /etc/apache2/sites-available/000-default.conf
+sudo echo "ServerAdmin webmaster@localhost" >> /etc/apache2/sites-available/000-default.conf
+sudo echo "DocumentRoot /var/www/html" >> /etc/apache2/sites-available/000-default.conf
+sudo echo "<Directory '/var/www/html'>" >> /etc/apache2/sites-available/000-default.conf
+sudo echo "Options FollowSymLinks" >> /etc/apache2/sites-available/000-default.conf
+sudo echo "AllowOverride All" >> /etc/apache2/sites-available/000-default.conf
+sudo echo "RewriteEngine on" >> /etc/apache2/sites-available/000-default.conf
+sudo echo "RewriteCond %%{REQUEST_FILENAME} -f [OR]" >> /etc/apache2/sites-available/000-default.conf
+sudo echo "RewriteCond %%{REQUEST_FILENAME} -d" >> /etc/apache2/sites-available/000-default.conf
+sudo echo "RewriteRule ^ - [L]" >> /etc/apache2/sites-available/000-default.conf
+sudo echo "RewriteRule ^ index.html [L]" >> /etc/apache2/sites-available/000-default.conf
+sudo echo "</Directory>" >> /etc/apache2/sites-available/000-default.conf
+sudo echo "</VirtualHost>" >> /etc/apache2/sites-available/000-default.conf
+sudo chmod 644 /etc/apache2/sites-available/000-default.conf
+sudo systemctl restart apache2
 sudo chown -R ubuntu:www-data /var/www
 sudo usermod -a -G www-data ubuntu
 echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
--- aws/server-init.sh
+++ aws/server-init.sh
@@ -5,7 +5,6 @@ echo "UBER_DB_PORT=${UBER_DB_PORT}" >> /etc/environment
 echo "UBER_DB_NAME=${UBER_DB_NAME}" >> /etc/environment
 echo "UBER_DB_USER=${UBER_DB_USER}" >> /etc/environment
 echo "UBER_DB_PASSWORD=${UBER_DB_PASSWORD}" >> /etc/environment
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
+sudo apt-get install -y npm
+sudo apt-get install -y nodejs
 # sudo scp -i ~/.ssh/aws-mac -r build/* ubuntu@52.23.214.166:/var/www/html
